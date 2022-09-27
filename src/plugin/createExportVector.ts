a8d7f3d281e6dd776aa4beda95137355ef245c25
Lorentz F. Barstad <29151133+lorentzfb@users.noreply.github.com>
2022-09-27 11:31:36 +0200

    Fix export layer error
    Fix bug where orphan elements was added to page
    when creating export layer.

--- src/plugin/createExportVector.ts
+++ src/plugin/createExportVector.ts
@@ -1,22 +1,11 @@
-import { AnnotationSymbol, SymbolValidationResult } from "./types";
+import {
+  AnnotationSymbol,
+  NodeId,
+  SupportedNodeType,
+  SupportedNodeTypes,
+  SymbolValidationResult,
+} from "./types";
 import { validateSymbol } from "./validation";
-
-const t: SceneNode["type"][] = [
-  "POLYGON",
-  "LINE",
-  "ELLIPSE",
-  "RECTANGLE",
-  "VECTOR",
-];
-
-type SupportedNodeType =
-  | LineNode
-  | RectangleNode
-  | EllipseNode
-  | PolygonNode
-  | VectorNode;
-
-type NodeId = string;
 
 export function createExportVectorFromNodeIdOrNodes(
   nodes: NodeId | ReadonlyArray<SceneNode>
@@ -63,35 +52,31 @@ function createExportVector(mainFrame: FrameNode, designGroup: GroupNode) {
   }
 
   // Create export nodes
-  const expNodes: SceneNode[] = [];
   const tmpVectorSuffix = "tmpExportVector";
 
   for (let i = 0; i < designGroup.children.length; i++) {
     const child = designGroup.children[i];
-    if (t.indexOf(child.type) == -1) continue;
-    const d = child as SupportedNodeType;
+    //console.log(child.type);
+    if (SupportedNodeTypes.indexOf(child.type) == -1) continue;
+    const node = child as SupportedNodeType;
 
-    let expNode: SceneNode | null = d.clone();
+    let expNode: SceneNode | null = null;
 
-    // Remove strokes if node has fill
-    if (Array.isArray(d.fills) && d.fills.length > 0) {
-      expNode.strokes = [];
-    }
-
-    // Outline any strokes except vector nodes
-    if (expNode.strokes.length > 0 && d.type !== "VECTOR") {
-      expNode = expNode.outlineStroke();
+    if (node.strokes.length > 0 && node.type !== "VECTOR") {
+      // Outline any strokes except vector nodes
+      expNode = node.outlineStroke();
+    } else {
+      expNode = node.clone();
+      // Remove strokes if node has fill
+      if (Array.isArray(node.fills) && node.fills.length > 0) {
+        expNode.strokes = [];
+      }
     }
 
     if (!expNode) continue;
 
     expNode.name += expNode.name + `_${tmpVectorSuffix}`;
-    expNodes.push(expNode);
-  }
-
-  // Add outlined nodes (vectors) to main frame
-  for (let i = 0; i < expNodes.length; i++) {
-    mainFrame.appendChild(expNodes[i]);
+    mainFrame.appendChild(expNode);
   }
 
   const tmpVectors = mainFrame.findChildren((c) => {
@@ -111,6 +96,6 @@ function createExportVector(mainFrame: FrameNode, designGroup: GroupNode) {
   symbolVector.y += designGroup.y;
 
   const fills = JSON.parse(JSON.stringify(symbolVector.fills));
-  fills[0].color = { r: 0, g: 0, b: 0 };
+  fills[0].color = { r: 0.137, g: 0.122, b: 0.125 };
   symbolVector.fills = fills;
 }
